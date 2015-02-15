74937153aadcf1b9e559f578c455156a07d599da
root <root@localhost.localdomain>
2015-02-14 18:31:56 -0600

    Hypervesterv1.0

---
+++ Hypervester.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash 
+
+#Hypervesterv1.0 tool for harvest email address From websites .
+#Enabled in two mode 
+# NAME : Hypervester
+# VERSION : Alpha 1.0
+#AUTHOR : Alexcerus HR
+# 
+#In first mode you have to create sitemap manually. You can use (http://www.xml-sitemaps.com/) to create sitemap.
+clear
+echo "
+                                             _                    _   ___  
+  /\  /\_   _ _ __   ___ _ ____   _____  ___| |_ ___ _ __  __   _/ | / _ \ 
+ / /_/ / | | | '_ \ / _ \ '__\ \ / / _ \/ __| __/ _ \ '__| \ \ / / || | | |
+/ __  /| |_| | |_) |  __/ |   \ V /  __/\__ \ ||  __/ |     \ V /| || |_| |
+\/ / /  \__, | .__/ \___|_|    \_/ \___||___/\__\___|_|      \_/ |_(_)___/ 
+   \/    |___/|_|                                         
+			         ~ Hypervester Alpha v1.0  By : Alexcerus HR ~                
+
+"
+echo -e "\e[93mVisible options :"
+
+echo -e "\e[97m
+1) - Generate sitemap of website than make name urllist.txt & Put in same directory [Fast Fast]
+2) - Generate sitemap than harvest email [Slow Automatique Mode]
+\e[92m"
+read m1
+if [ "$m1" = "1" ];then
+echo "
+[+] The Script start working,be Patient .....
+"
+cat urllist.txt | while read f1
+do
+
+w3m $f1 >> f1
+perl -wne'while(/[\w\.]+@[\w\.]+/g){print "$&\n"}' f1 | sort -u >> output.txt
+rm f1
+done
+
+cat output.txt
+echo "
+Harvesting is complete.Open output.txt file to view email address.
+"
+fi
+
+if [ "$m1" = "2" ];then
+echo -e "\e[91m
+[+] \e[97mEnter Website To Harvest Email Address 
+[~] Example : http://test.com \e[92m
+"
+read choice
+echo -e "
+\e[91m[+] \e[97mThe script start making urllist of website.So be Patient....
+"
+wget --spider --recursive --no-verbose --output-file=wgetlog.txt "$choice"
+sed -n "s@.\+ URL:\([^ ]\+\) .\+@\1@p" wgetlog.txt | sed "s@&@\&amp;@" > urllist.txt
+rm wgetlog.txt
+cat urllist.txt | while read f1
+do
+w3m $f1 >> f1
+perl -wne'while(/[\w\.]+@[\w\.]+/g){print "$&\n"}' f1 | sort -u >> output.txt
+rm f1
+done
+
+cat output.txt
+echo "
+Harvesting is complete. Open output.txt file to view email address.
+"
+fi
---
+++ README.md
@@ -0,0 +1,18 @@
+THE Readme file ~
+============
+Hypervester Alpha v1.0 ~
+This should give you a general overview of what the tool is all about and how to go about using it. This is a the 
+result of a project of mine to come up with better solution for gathering informations URLs for research and I tweaked it to align with 
+my hacking needs. I also decided I was going to do everything in 
+bash to make things easy on myself, well cause thats how I am :p
+
+Hypervester is simple tool created for extracting emails from website or given domain .
+
+The tool Enabled in two mode fast & slow. In first mode you have to specify sitemap of website .
+Second mode is automatic .But it is slow :p 
+
+i Get some help from the Original Harvester tool ~~
+
+Usage of script :-
+chmod +x Hypervester.sh
+./Hypervester.sh
